59162229937ced75bcbb9496a98ce05f1c3140f9
Karthik Vegesna <krvegesna@gmail.com>
2016-07-12 14:41:06 -0700

    Update graphprac.py

--- graphprac.py
+++ graphprac.py
@@ -3,40 +3,64 @@ import matplotlib.pyplot as plt
 
 with open('output.txt','r') as f:
     lines = f.read().split("\n")
-
+    lines1 = f.read().split("\n")
 
 print("Number of lines is {}".format(len(lines)))
 
-word = '2.00' # dummy word. you take it from input
+word = '2.00'
+word1 = '4.00'
+word2 = '6.00'
+word3 = '8.00'
+word4 = '10.0'
 
-# iterate over lines, and print out line numbers which contain
-# the word of interest.
 for i,line in enumerate(lines):
     if word in line: # or word in line.split() to search for full words
         print("Word \"{}\" found in line {}".format(word, i+1))
-        str= line
+        str1= line
         print (str)
         str2 = line.split()[5]
         print (line.split()[5])
-        variable = []
-        break
+
+    if word1 in line:
+        print("Word \"{}\" found in line {}".format(word1, i+1))
+        str3= line
+        print (str)
+        str4 = line.split()[5]
+        print (line.split()[5])
+
+    if word2 in line:
+        print("Word \"{}\" found in line {}".format(word2, i+1))
+        str5= line
+        print (str)
+        str6 = line.split()[5]
+        print (line.split()[5])
+
+    if word3 in line:
+        print("Word \"{}\" found in line {}".format(word3, i+1))
+        str7= line
+        print (str)
+        str8 = line.split()[5]
+        print (line.split()[5])
+
+    if word4 in line:
+        print("Word \"{}\" found in line {}".format(word4, i+1))
+        str9= line
+        print (str)
+        str10 = line.split()[5]
+
 
 x = [2, 4, 6, 8, 10]
-y = [ [0, 10, 20, 30, 90], [50, 60, 70, 80, 90]]
+y = [[str2, str4, str6, str8, str10], [5, 6, 7, 8, 9]]
 csfont = {'fontname':'Helvetica Neue'}
 labels = ['foo', 'bar', 'baz']
 csfont = {'fontname':'Helvetica Neue'}
 plt.title('Advantages of Bandwidth Limiting using iPerf and 10 MB Limit',**csfont)
 plt.xlabel('Bandwidth Limit in MBps', **csfont)
 plt.ylabel('Throughput in Mbps', **csfont)
-labels = ['With Queues', 'Without Queues']
+labels = ['Without Queues', 'Without Queues']
 
 for y_arr, label in zip(y, labels):
     plt.plot(x, y_arr, label=label)
 
 plt.legend()
 plt.show()
-
-
-
-
