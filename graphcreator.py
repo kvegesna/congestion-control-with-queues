677475fc67d7bae62c1f90e2b4befac494adacd2
Karthik Vegesna <krvegesna@gmail.com>
2016-07-15 09:49:03 -0700

    Update graphcreator.py

--- graphcreator.py
+++ graphcreator.py
@@ -5,14 +5,14 @@ with open('output.txt', 'r') as f:
     lines = f.read().split("\n")
     lines1 = f.read().split("\n")
 
-print("Number of lines is {}".format(len(lines)))
+#print("Number of lines is {}".format(len(lines)))
 
 word0 = '0.00-1.00'
 word = '5.00-6.00'
-word1 = '20.00-21.00'
-word2 = '35.00-36.00'
-word3 = '50.00-51.00'
-word4 = '65.00-66.00'
+word1 = '27.00-28.00'
+word2 = '43.00-44.00'
+word3 = '60.00-61.00'
+word4 = '70.00-71.00'
 
 for i, line in enumerate(lines):
     if word0 in line:  # or word in line.split() to search for full words
@@ -48,6 +48,7 @@ for i, line in enumerate(lines):
             holder = lines[i]
             strholder = holder.split()[4]
             fltstrholder = float(strholder)
+
             if (fltstrholder < 10):
                 fltstrholder *= 1000
             total += fltstrholder
@@ -56,29 +57,29 @@ for i, line in enumerate(lines):
         #print(line.split()[4])
         #print(total)
         avg = total / 10000
+        #print(avg)
         x = 0
         total = 0
 
     if word1 in line:
-        print("Word \"{}\" found in line {}".format(word1, i + 1))
-        print("x is ")
-        print(x)
+        #print("Word \"{}\" found in line {}".format(word1, i + 1))
+        #print("x is ")
+        #print(x)
         str3 = line
-        print(str)
+        #print(str)
         str4 = line.split()[5]
-        print(line.split()[5])
+        #print(line.split()[5])
         while x < 10:
             holder = lines[i]
             strholder = holder.split()[4]
             fltstrholder = float(strholder)
-            print(fltstrholder)
-
+            #print(fltstrholder)
             total += fltstrholder
             x += 1
             i += 1
         avg1 = total / 10000
         x = 0
-        print(avg1)
+        #print(avg1)
         total = 0
 
     if word2 in line:
@@ -87,11 +88,11 @@ for i, line in enumerate(lines):
         print(str)
         str6 = line.split()[5]
         print(line.split()[5])
-        while x < 11:
+        while x < 10:
             holder = lines[i]
             strholder = holder.split()[4]
             fltstrholder = float(strholder)
-
+            print (fltstrholder)
             total += fltstrholder
             x += 1
             i += 1
@@ -119,14 +120,9 @@ for i, line in enumerate(lines):
         print(avg3)
         total = 0
 
-    if word4 in line:
-        print("Word \"{}\" found in line {}".format(word4, i + 1))
-        str9 = line
-        print(str)
-        str10 = line.split()[5]
 
-x = [0, 2, 4, 6, 8, 10]
-y = [[avg0, avg, avg1, avg2, avg3, 0.5], [0, 0.1, 0.2, 0.3, 0.4, 1.0]]
+x = [0, 2, 4, 6, 8]
+y = [[avg0, avg, avg1, avg2, avg3], [0, 0.1, 0.2, 0.3, 0.4, ]]
 csfont = {'fontname': 'Helvetica Neue'}
 plt.title('Advantages of Bandwidth Limiting using iPerf and 10 MB Limit', **csfont)
 plt.xlabel('UDP Congestion(MBps)', **csfont)
@@ -138,5 +134,7 @@ for y_arr, label in zip(y, labels):
 
 plt.legend()
 plt.show()
+
+
 
 
